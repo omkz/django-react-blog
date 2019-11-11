10b3e886a56f81df437f206198868533779433a6
Kurnia Muhamad <omenkzz@gmail.com>
2019-11-11 10:50:03 +0700

    Fix Post logic

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from blog.serializers import UserSerializer, GroupSerializer, PostSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import permissions
-from rest_framework.response import Response
-
-
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
@@ -17,20 +13,10 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
 
     queryset = Post.objects.filter(is_public__exact=True)
     serializer_class = PostSerializer
 
-    def get_permissions(self):
-
-        if self.action == 'list':
-            permission_classes = [permissions.AllowAny,]
-        else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
-
-
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-
