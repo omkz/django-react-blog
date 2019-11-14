4e679f68e5a45eeb8fbbe291f794f4703c2603af
Kurnia Muhamad <omenkzz@gmail.com>
2019-11-14 13:19:51 +0700

    Refactor Appjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,69 +32,68 @@ class App extends React.Component {
         let nav;
         if (currentUser) {
             nav = <nav className="navbar" role="navigation" aria-label="main navigation">
-                    <div className="navbar-brand">
-                        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
-                           data-target="navbarBasicExample">
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                        </a>
+                <div className="navbar-brand">
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                       data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                        <Link to="/" className="navbar-item">Home</Link>
+                        <Link to="/mypost" className="navbar-item">My Post</Link>
+                        <Link to="/createpost" className="navbar-item">Create Post</Link>
                     </div>
 
-                    <div id="navbarBasicExample" className="navbar-menu">
-                        <div className="navbar-start">
-                            <Link to="/" className="navbar-item">Home</Link>
-                            <Link to="/mypost" className="navbar-item">My Post</Link>
-                            <Link to="/createpost" className="navbar-item">Create Post</Link>
-                        </div>
-
-                        <div className="navbar-end">
-                            <div className="navbar-item">
-                                <div className="buttons">
-                                    <Link to="/login" className="button is-primary">Logout</Link>
-                                </div>
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                <Link to="/login" className="button is-primary">Logout</Link>
                             </div>
                         </div>
                     </div>
-                </nav>
+                </div>
+            </nav>
         } else {
-             nav = <nav className="navbar" role="navigation" aria-label="main navigation">
-                    <div className="navbar-brand">
-                        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
-                           data-target="navbarBasicExample">
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                        </a>
+            nav = <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                       data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                        <Link to="/" className="navbar-item">Home</Link>
                     </div>
 
-                    <div id="navbarBasicExample" className="navbar-menu">
-                        <div className="navbar-start">
-                            <Link to="/" className="navbar-item">Home</Link>
-                        </div>
-
-                        <div className="navbar-end">
-                            <div className="navbar-item">
-                                <div className="buttons">
-                                    <Link to="/login" className="button is-primary">Login</Link>
-                                </div>
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                <Link to="/login" className="button is-primary">Login</Link>
                             </div>
                         </div>
                     </div>
-                </nav>
+                </div>
+            </nav>
         }
 
         return (
             <div>
-                 {nav}
+                {nav}
                 <Switch>
                     <Route exact path="/" component={AllPost}/>
                     <Route path="/login" component={Login}/>
                     <Route path="/signup" component={SignUp}/>
-                    <PrivateRoute authenticated={this.state.isAuthenticated} path="/createpost" component={CreatePost}/>
-                    <PrivateRoute authenticated={this.state.isAuthenticated} path="/mypost" component={MyPost}/>
-                    <PrivateRoute authenticated={this.state.isAuthenticated} path='/edit/:id' component={EditPost}/>
-
+                    <PrivateRoute path="/createpost" component={CreatePost}/>
+                    <PrivateRoute path="/mypost" component={MyPost}/>
+                    <PrivateRoute path='/edit/:id' component={EditPost}/>
                 </Switch>
             </div>
         )
